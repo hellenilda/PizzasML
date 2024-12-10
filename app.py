fb9917e30c132f4abcde4f7375338e484e241306
hellenilda <hellen95limaaraujo@gmail.com>
2024-12-10 16:38:33 -0300

    :sparkles: feat: modelo treinado e deploy web via Streamlit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("datasets/pizzas.csv")
+
+modelo = LinearRegression()
+
+# Treinando o modelo
+x = df[["diametro"]]
+y = df[["preco"]]
+
+st.title("Prevendo o valor de uma pizza")
+st.divider()
+
+diametro = st.number_input("Digite o tamanho do diâmetro da pizza: ")
+
+if diametro:
+    preco_previsto = modelo.predict([[diametro]])[0][0]
+    st.write(f"A pizza com diâmetro de {diametro} custa R$ {preco_previsto:.2f}")
